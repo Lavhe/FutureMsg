8adaa52d61b8cc79abcf3f3fff2244a8dc0dc6d4
Lavhe <mulavhe@gmail.com>
2017-07-15 21:54:54 +0200

    Create Project

--- FutureMsg/Resources/Resource.Designer.cs
+++ FutureMsg/Resources/Resource.Designer.cs
@@ -57,6 +57,31 @@ namespace FutureMsg
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f060001
+			public const int linearLayout1 = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int linearLayout2 = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int linearLayout3 = 2131099651;
+			
+			// aapt resource value: 0x7f060000
+			public const int tabHost1 = 2131099648;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -88,6 +113,22 @@ namespace FutureMsg
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Theme_Material = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
